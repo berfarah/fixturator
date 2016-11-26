beb649a3df06e00175a10fe78d3558aad5061e59
Bernardo Farah <ber@bernardo.me>
2016-11-25 23:09:05 -0600

    Make tests handle both Rails 5 and 4.2

--- test/dummy/db/migrate/20161120021029_create_users.rb
+++ test/dummy/db/migrate/20161120021029_create_users.rb
@@ -1,4 +1,8 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
+superclass = ActiveRecord::Migration
+# TODO: Inherit from the 5.0 Migration class directly when we drop support for Rails 4.
+superclass = ActiveRecord::Migration[5.0] if superclass.respond_to?(:[])
+
+class CreateUsers < superclass
   def change
     create_table :users do |t|
       t.string :name
--- test/dummy/db/migrate/20161120030818_create_posts.rb
+++ test/dummy/db/migrate/20161120030818_create_posts.rb
@@ -1,4 +1,8 @@
-class CreatePosts < ActiveRecord::Migration[5.0]
+superclass = ActiveRecord::Migration
+# TODO: Inherit from the 5.0 Migration class directly when we drop support for Rails 4.
+superclass = ActiveRecord::Migration[5.0] if superclass.respond_to?(:[])
+
+class CreatePosts < superclass
   def change
     create_table :posts do |t|
       t.string :title
